a773429835a16026b9ca665fa8f5c0cda0b3fa7b
Marcus Östling <mpt.ostling@gmail.com>
2023-12-15 01:49:40 +0100

    remove unused code

--- src/operations2/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod operation;
-pub mod list_operation;
-pub mod add_operation;
-pub mod remove_operation;
-pub mod done_operation;
-pub mod clear_operation;
-
-pub use operation::*;
--- src/operations2/operation.rs
+++
@@ -1,7 +0,0 @@
-use crate::operations::OperationError;
-
-pub trait Operation {
-    fn execute(&self, lines: Vec<String>) -> Result<Vec<String>, OperationError>;
-    fn description(&self) -> String;
-}
-
